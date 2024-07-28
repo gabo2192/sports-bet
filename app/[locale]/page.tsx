ceb95f9ccb3d55954d1132ac7d737033bf3497bf
Gabriel Rojas <35940919+gabo2192@users.noreply.github.com>
2024-07-28 15:52:38 -0500

    feat: update homepage

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,27 @@
 import { MainLayout } from "@/components/layouts/main-layout";
+import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
-  const t = useTranslations("HomePage");
+  const t = useTranslations("homepage");
 
-  return <MainLayout>{t("title")}</MainLayout>;
+  return (
+    <MainLayout>
+      <section className="flex gap-4 flex-col items-start mt-10 border-b pb-10">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          {t("title")}
+        </h1>
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          {t("subtitle")}
+        </h3>
+        <Button>Comenzar ahora!</Button>
+      </section>
+      <section className="mt-10">
+        <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          {t("feature.title")}
+        </h2>
+        <ul className="scroll-m-20"></ul>
+      </section>
+    </MainLayout>
+  );
 }
